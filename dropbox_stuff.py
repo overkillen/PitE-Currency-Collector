6d11afd5d7ac186f855c707b46e0a89b2cfe12d9
tulowiecki <pawtul@gmail.com>
2017-05-22 18:46:01 +0200

    Added dropbox integration

---
+++ dropbox_stuff.py
@@ -0,0 +1,26 @@
+import json
+
+import dropbox
+
+
+class DBclient:
+    token = '4F7nibHK-YAAAAAAAAAABqk5WWYBQYAdXBDkTdg4LImkLa0GE3IQ0b7iyVcKPdT3'
+
+    @property
+    def client(self):
+        if not hasattr(self, '_client'):
+            self._client = dropbox.Dropbox(self.token)
+        return self._client
+
+    def __init__(self, token=None):
+        if token is not None:
+            self.token = token
+
+    def upload_file(self, content, filename='/currency_dump.json'):
+        self.client.files_upload(content.encode("utf-8"), filename)
+        return True
+
+    def download_file(self, filename='/currency_dump.json'):
+        metadata, resp = self.client.files_download(filename)
+        return resp.content.decode("utf-8")
+
